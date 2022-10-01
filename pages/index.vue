7e1a94d06a0d7ff6f5629746aff1fdc2e847d3dc
do-ockhyun <do-ockhyun@naver.com>
2022-10-02 08:30:48 +0900

    프로젝트 생성

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div>
+    <el-button>Default</el-button>
+    <el-icon>
+      <User style="font-size: 24px" />
+    </el-icon>
+  </div>
+</template>
+
+<style scoped></style>
